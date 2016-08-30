7beeaeb51f93510784ba2f03d0a2c05c218bf144
Taylor Page <jonathantaylorpage@gmail.com>
2016-08-29 22:06:44 -0500

    (chore) Create basic mockup of Translator component

--- client/components/translator/Translator.js
+++ client/components/translator/Translator.js
@@ -8,7 +8,24 @@ export default class Translator extends React.Component {
   render() {
     return (
       <div>
-        Translator
+        <div className="col-xs-12">
+          <input className="block-center text-inputs" type="text"></input>
+          <select>
+            <option value="English">English</option>
+            <option value="Spanish">Spanish</option>
+            <option value="French">French</option>
+            <option value="German">German</option>
+          </select>
+        </div>
+        <div className="col-xs-12">
+          <input className="text-center text-inputs" type="text"></input>
+          <select>
+            <option value="English">English</option>
+            <option value="Spanish">Spanish</option>
+            <option value="French">French</option>
+            <option value="German">German</option>
+          </select>
+        </div>
       </div>
     )
   }
