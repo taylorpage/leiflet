e2e4b5c649fd907be5056dc22ab890844b0edfee
Taylor Page <jonathantaylorpage@gmail.com>
2016-08-30 08:40:33 -0500

    (chore) Allow text input to be saved to state

--- client/components/translator/Translator.js
+++ client/components/translator/Translator.js
@@ -3,13 +3,32 @@ import React from 'react';
 export default class Translator extends React.Component {
   constructor(props){
     super(props);
+
+    this.state = {
+      userPhrase: '',
+      clientPhrase: ''
+    }
+  }
+
+  handleUserChange() {
+    this.setState({
+      userPhrase: document.getElementById('user').value
+    });
+    console.log(this.state);
+  }
+
+  handleClientChange() {
+    this.setState({
+      clientPhrase: document.getElementById('client').value
+    });
+    console.log(this.state);
   }
 
   render() {
     return (
       <div>
         <div className="col-xs-12">
-          <input className="block-center text-inputs" type="text"></input>
+          <input className="text-inputs" type="text" id="user" onChange={ this.handleUserChange.bind(this) }></input>
           <select>
             <option value="English">English</option>
             <option value="Spanish">Spanish</option>
@@ -18,7 +37,7 @@ export default class Translator extends React.Component {
           </select>
         </div>
         <div className="col-xs-12">
-          <input className="text-center text-inputs" type="text"></input>
+          <input className="text-inputs" type="text" id="client" onChange={ this.handleClientChange.bind(this) }></input>
           <select>
             <option value="English">English</option>
             <option value="Spanish">Spanish</option>
