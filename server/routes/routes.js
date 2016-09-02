52156934d52dea337582fab63e7b294d9b5989f5
Taylor Page <jonathantaylorpage@gmail.com>
2016-09-01 22:17:58 -0500

    (feat) Allow translated text to appear on screen with conversation component

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,14 +4,12 @@ module.exports = function(app, express) {
 
   app.post('/translate/client', function(req, res) {
     helpers.translator(req.body.phrase, req.body.to, req.body.from, function(data) {
-      console.log(data);
       res.status(200).send(data);
     });
   })
 
   app.post('/translate/user', function(req, res) {
     helpers.translator(req.body.phrase, req.body.to, req.body.from, function(data) {
-      console.log(data);
       res.status(200).send(data);
     });
   })
